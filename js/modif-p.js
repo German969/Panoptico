ba1943d9ab0ed706df6bdbdbcecb7941911156e4
German Monzon <german.monzon.95@gmail.com>
2017-06-20 13:20:29 -0300

    Corregido js de admin-presupuesto
    1- numeros de fila estaban mal
    2- nombre de la funcion incorrecto
    3- nombre de los archivos js incorrectos

--- js/modif-p.js
+++ js/modif-p.js
@@ -1,14 +1,14 @@
-function modp(fila) {
+function modp2(fila) {
 	
-	v1 = document.getElementById('f' + fila + 'c2').innerHTML;
-	v2 = document.getElementById('f' + fila + 'c3').innerHTML;
-	v3 = document.getElementById('f' + fila + 'c4').innerHTML;
-    v4 = document.getElementById('f' + fila + 'c5').innerHTML;
+	v1 = document.getElementById('f' + fila + 'c1').innerHTML;
+	v2 = document.getElementById('f' + fila + 'c2').innerHTML;
+	v3 = document.getElementById('f' + fila + 'c3').innerHTML;
+    v4 = document.getElementById('f' + fila + 'c4').innerHTML;
 	
-	document.getElementById('f' + fila + 'c2').innerHTML = '<input type="text" id="vf' + fila + 'c2" value="' + v1 + '"/>';
-	document.getElementById('f' + fila + 'c3').innerHTML = '<input type="text" id="vf' + fila + 'c3" value="' + v2 + '"/>';
-	document.getElementById('f' + fila + 'c4').innerHTML = '<input type="text" id="vf' + fila + 'c4" value="' + v3 + '"/>';
-    document.getElementById('f' + fila + 'c5').innerHTML = '<input type="text" id="vf' + fila + 'c4" value="' + v4 + '"/>';
+	document.getElementById('f' + fila + 'c1').innerHTML = '<input type="text" id="vf' + fila + 'c1" value="' + v1 + '"/>';
+	document.getElementById('f' + fila + 'c2').innerHTML = '<input type="text" id="vf' + fila + 'c2" value="' + v2 + '"/>';
+	document.getElementById('f' + fila + 'c3').innerHTML = '<input type="text" id="vf' + fila + 'c3" value="' + v3 + '"/>';
+    document.getElementById('f' + fila + 'c4').innerHTML = '<input type="text" id="vf' + fila + 'c4" value="' + v4 + '"/>';
 	document.getElementById('M' + fila).innerHTML = '<input type="button" class="btn btn-primary" value="Aceptar" onclick="yata(' + fila + ')">';
 	
 }
